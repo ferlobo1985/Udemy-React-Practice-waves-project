3f7a074e04d4ba072af673fc083e34e6f91e47f0
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-03-22 19:39:40 -0300

    05- Connecting to mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const routes = require('./routes')
+
+
+// mongodb+srv://admin:<password>@cluster0.kbuow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true,
+    useFindAndModify: false
+});
+
+
 
 /// body parse
 app.use(express.json())
