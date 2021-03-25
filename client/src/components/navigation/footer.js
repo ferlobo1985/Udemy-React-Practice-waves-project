37d90e31782203d0f500805bad26d3001012d678
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-03-25 03:14:19 -0300

    36-Creating the header and the footer

--- client/src/components/navigation/footer.js
+++ client/src/components/navigation/footer.js
@@ -1,11 +1,65 @@
 import React from 'react';
 
+import ContactsIcon from '@material-ui/icons/Contacts';
+import TimelapseIcon from '@material-ui/icons/Timelapse';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
+//import { useSelector } from 'react-redux';
+
 const Footer = () => {
+    
 
     return(
-        <>
-            Footer
-        </>
+        <footer className="bck_b_dark">
+            <div className="container">
+                <div className="logo">
+                    WAVES
+                </div>
+                <div className="wrapper">
+                    <div className="left">
+                        <h2>Contact information</h2>
+                        <div className="business_nfo">
+                            <div className="tag">
+                                <ContactsIcon/>
+                                <div className="nfo">
+                                    <div>Address</div>
+                                    <div>Some street 222</div>
+                                </div>
+                            </div>
+                            <div className="tag">
+                                <TimelapseIcon/>
+                                <div className="nfo">
+                                    <div>Phone</div>
+                                    <div>27287912323</div>
+                                </div>
+                            </div>
+                            <div className="tag">
+                                <PhoneIcon/>
+                                <div className="nfo">
+                                    <div>Working hours</div>
+                                    <div>always closed</div>
+                                </div>
+                            </div>
+                            <div className="tag">
+                                <EmailIcon/>
+                                <div className="nfo">
+                                    <div>Email</div>
+                                    <div>waves@gmail.com</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="left">
+                        <h2>Be the first to know</h2>
+                        <div>
+                            <div>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 
 }
--- client/src/components/navigation/header.js
+++ client/src/components/navigation/header.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
     return(
-        <>
-            Header
-        </>
+        <header className="bck_b_light">
+            <div className="container">
+                <div className="left">
+                    <div className="logo">
+                        WAVES
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="top">
+                        <>
+                            <div className="cart_link">
+                                <span>1</span>
+                                <Link to="/dashboard/user/user_cart">
+                                    My cart
+                                </Link>
+                            </div>
+
+                            <Link to="/dashboard">
+                                   My account
+                            </Link>
+                            <span 
+                                onClick={()=> alert('log out')}
+                            >
+                                Log out
+                            </span>
+
+                            <Link to="/sign_in">
+                                Log in
+                            </Link>
+                        </>
+                    </div>
+                    <div className="bottom">
+                        <Link to="/">
+                            Home
+                        </Link>
+                        <Link to="/shop">
+                            Shop
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 
 }
