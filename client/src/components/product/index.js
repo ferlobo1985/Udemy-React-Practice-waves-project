21ed0e1f227bf8ec888dc69652365a2f33f5d383
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-03-31 03:52:08 -0300

    79-Product carrousel

--- client/src/components/product/index.js
+++ client/src/components/product/index.js
@@ -8,11 +8,31 @@ import { clearCurrentProduct } from 'store/actions'
 import { renderCardImage } from 'utils/tools'
 import Loder from 'utils/loader';
 
+import { Modal } from 'react-bootstrap';
+import Slider from 'react-slick';
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 
 const ProductDetail = (props) => {
+    const [modal, setModal ] = useState(false);
     const products = useSelector(state=> state.products);
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const sliderSetting = {
+        dot:false,
+        infinite:true,
+        speed:500,
+        slidesToShow:1,
+        slidesToScroll:1
+    }
+
+    const handleClose = () => setModal(false);
+    const handleCarrousel = () => {
+        if(products.byId.images.length > 0){
+            setModal(true);
+        }
+    }
 
 
     useEffect(()=>{
@@ -42,7 +62,7 @@ const ProductDetail = (props) => {
                                 <img
                                     alt="some alt"
                                     src={renderCardImage(products.byId.images)}
-                                    onClick={()=> alert('show carrousel')}
+                                    onClick={()=> handleCarrousel()}
                                 >
                                 </img>
                             </div>
@@ -56,9 +76,29 @@ const ProductDetail = (props) => {
                 :
                     <Loder/>
                 }
-
-
             </div>
+
+
+            <Modal show={modal} onHide={handleClose} dialogClassName="modal-90w">
+                <Modal.Header closeButton></Modal.Header>      
+                <Modal.Body>
+                    <Slider {...sliderSetting}>
+                        { products.byId && products.byId.images ?
+                            products.byId.images.map((item)=>(
+                                <div key={item} style={{margin:'0 auto'}}>
+                                    <div className="img-block"
+                                        style={{
+                                            background:`url(${item}) no-repeat`
+                                        }}
+                                    ></div>
+                                </div>
+                            ))
+                        :null
+                        }
+                    </Slider>
+                </Modal.Body>           
+            </Modal>
+
         </div>
     )
 }
